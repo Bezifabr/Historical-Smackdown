22cc98ecdaeda2a9f9f742a28741b24ba6c3e75c
Bezifabr <Bezifabr@gmail.com>
2017-05-20 00:17:30 +0200

    Removed unnecessary variable from Game class

--- include/Game.h
+++ include/Game.h
@@ -1,11 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 #include <string>
 
+
 class Game {
-	std::string title;
 	sf::RenderWindow window;
 public:
 	void CreateWindow(const std::string& title);
